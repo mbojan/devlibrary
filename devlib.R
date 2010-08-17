9d1a598f81a04dc9a500aa041b5877fd675f630a
Michal Bojanowski <michal2992@gmail.com>
2010-08-17 17:15:22 +0000

    finished writing main functions

--- devlib.R
+++ devlib.R
@@ -7,21 +7,28 @@
 
 dlib1 <- "/home/mbojan/lib/R/dev"
 dlib2 <- "/home/mbojan/R/src/devlib/lib"
-options(devlib =dlib2 )
+options(devlib =dlib1 )
+
+path <- dlib1
 
 #============================================================================ 
 # build db of packages in dev library
 
 buildLibDB <- function(path=getOption("devlib"))
 {
+  # absolute path
   path <- path.expand(path)
-  l <- list.files(path)
-  ll <- lapply(l, function(n) list.files(file.path(path, n)))
-  names(ll) <- l
-  db <- data.frame( package=rep(names(ll), sapply(ll, length)), 
-      version=unlist(ll, use.names=FALSE),
+  # package names
+  pkgpaths <- list.files(path, full.names=TRUE)
+  isdir <- file.info(pkgpaths)$isdir
+  pkgnames <- basename( pkgpaths[isdir] )
+  vernames <- lapply( pkgpaths[isdir], list.files)
+  ind <- sapply(vernames, length)
+  db <- data.frame( 
+      package=rep(pkgnames, ind),
+      version=unlist(vernames, use.names=FALSE),
+      libloc = rep(pkgpaths[isdir], ind),
       stringsAsFactors=FALSE )
-  db$libloc <- apply(db, 1, function(v) file.path(path, v[1], v[2]))
   db$ver <- package_version(db$version)
   db
 }
@@ -30,17 +37,12 @@ buildLibDB <- function(path=getOption("devlib"))
 #============================================================================ 
 # install packages to dev tree
 
-# TODO check if exists, add force argument?
-
-# TODO detach/unload if loaded?
-
 install.devlib <- function(fname, pkg, ver, ...)
 {
+  # path to library
   loc <- file.path(getOption("devlib"), pkg, ver)
-  if( !file.exists(loc) )
-  {
+  if(!file.exists(loc)) 
     dir.create(loc, recursive=TRUE)
-  }
   cat("Trying to install package", sQuote(fname), "to", loc, "\n")
   install.packages(fname, lib=loc, repos=NULL, ...)
 }
@@ -49,14 +51,46 @@ install.devlib <- function(fname, pkg, ver, ...)
 #============================================================================ 
 # loading packages from dev library
 
-devlibrary <- function(pkg, ver, devtree=getOption("devlib"), ...)
+# TODO iv ver=NULL, load the latest
+
+# if package not found list those available
+
+devlib <- function(pkg, ver=NULL, devtree=getOption("devlib"), ...)
 {
-  loc <- file.path(devtree, pkg, ver)
+  # check if availability
+  db <- buildLibDB(devtree)
+  pkgmatch <- db$package %in% pkg
+  if(!any(pkgmatch))
+    stop("package", pkg, "not available in", devtree)
+  dbsub <- db[pkgmatch,]
+  if(is.null(ver))
+  {
+    # load latest
+    loadver <- dbsub$version[ which(dbsub$ver == max(dbsub$ver)) ]
+  } else
+  {
+    # test if version available
+    vermatch <- dbsub$version %in% ver
+    if(!any(vermatch))
+    {
+      stop("package", paste(pkg, ver, sep="_"), "not available\n",
+          "available versions of", pkg, ":", paste(dsub$version, collapse=", "))
+    }
+    loadver <- ver
+  }
+  loc <- file.path(devtree, pkg, loadver)
   if( !file.exists(loc) )
     stop("library ", loc, " does not exist")
-  cat("Loading package", pkg, "from", loc, "\n")
+  if( pkg %in% unique(c(.packages(), loadedNamespaces())) )
+  {
+    cat("Trying to unload/detach package", sQuote(pkg), "\n")
+    detach( paste("package", "network", sep=":"), unload=TRUE, character.only=TRUE)
+  }
+  cat("Loading package", sQuote(pkg), paste("(", loadver, ")", sep=""),
+      "from", devtree, "\n")
   library(pkg, character.only=TRUE, lib.loc=loc, ...)
 }
+
 
 #============================================================================ 
 # adding dev library to lib paths
